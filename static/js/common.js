b3531fcaa4156376a90e30efe0cc9a07937e9410
yuta adachi <yuiga1234@yahoo.co.jp>
2018-03-08 19:21:25 +0900

    youtube動画のパラメータでの制御を検証する。

--- static/js/common.js
+++ static/js/common.js
@@ -55,9 +55,42 @@ $(function(){
     });
 
 
+  var ua = navigator.userAgent;
+  if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+    // スマートフォン用コード
+    $('#video').append('<p>あああああ</p>');
+  } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+    // タブレット用コード
+  } else {
+    // PC用コード
+    $('#video').append('<iframe width="1000" height="500" src="https://www.youtube.com/embed/mQ0Clc2F-pY?autoplay=1&loop=1&playlist=mQ0Clc2F-pY&rel=0&showinfo=0&controls=0&modestbranding=1&disablekb=1&iv_load_policy=3" frameborder="0"></iframe>');
+  }
 
 
 
+
+});
+
+
+
+$(window).on("load resize", function () {
+  var windowWidth = parseInt($(window).width());
+
+  // var $video = $('#video');
+  // if (windowWidth < 768) {
+  //   // if ($video.hasClass('spVideo')) {
+  //   if ($video.hasClass('pcVideo')) {
+  //     $video.toggleClass('pcVideo', 'spVideo');
+  //     $video.append('<p>あああああ</p>');
+  //   }
+  //   // }
+  // } else {
+  //   if ($video.hasClass('pcVideo')) {
+
+  //   } else {
+  //     // $video.append('<iframe width="1000" height="500" src="https://www.youtube.com/embed/mQ0Clc2F-pY?autoplay=1&loop=1&playlist=mQ0Clc2F-pY&rel=0&showinfo=0&controls=0&modestbranding=1&disablekb=1&iv_load_policy=3" frameborder="0"></iframe>');
+  //   }
+  // }
 
 
 });
